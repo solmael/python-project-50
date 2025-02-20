2ed77f2ffdf5b0fb334f43111b2fb51a40012e82
solmael <solmael13@gmail.com>
2025-02-20 12:16:32 +0300

    reader for json and yaml

---
+++ gendiff/reader.py
@@ -0,0 +1,16 @@
+import json
+import yaml
+
+
+def parse_file(file_path):
+    with open(file_path, 'r') as file:
+        if file_path.endswith('.json'):
+            return json.load(file)
+        else:
+            return yaml.safe_load(file)
+
+
+def read_files(file1_path, file2_path):
+    file1_data = parse_file(file1_path)
+    file2_data = parse_file(file2_path)
+    return file1_data, file2_data
