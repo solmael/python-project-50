1f0b9bda80324ae0ac91cdf025606964f619fe63
solmael <solmael13@gmail.com>
2025-02-21 19:51:27 +0300

    mod generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -8,14 +8,23 @@ def generate_diff(file1_path, file2_path):
     
     diff = []
     for key in all_keys:
+        value1 = format_value(file1_data.get(key))
+        value2 = format_value(file2_data.get(key))
+        
         if key not in file1_data:
-            diff.append(f"  + {key}: {file2_data[key]}")
+            diff.append(f'  + {key}: {value2}')
         elif key not in file2_data:
-            diff.append(f"  - {key}: {file1_data[key]}")
+            diff.append(f'  - {key}: {value1}')
         elif file1_data[key] == file2_data[key]:
-            diff.append(f"    {key}: {file1_data[key]}")
+            diff.append(f'    {key}: {value1}')
         else:
-            diff.append(f"  - {key}: {file1_data[key]}")
-            diff.append(f"  + {key}: {file2_data[key]}")
+            diff.append(f'  - {key}: {value1}')
+            diff.append(f'  + {key}: {value2}')
     
-    return "{\n" + "\n".join(diff) + "\n}"
+    return '{\n' + '\n'.join(diff) + '\n}'
+
+
+def format_value(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    return str(value)
