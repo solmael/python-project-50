dfcce08801318b82961444d923adf4f9762cffed
solmael <solmael13@gmail.com>
2025-02-23 19:04:15 +0300

    upd reader

--- gendiff/reader.py
+++ gendiff/reader.py
@@ -7,7 +7,7 @@ def parse_file(file_path):
     with open(file_path, 'r') as file:
         if file_path.endswith('.json'):
             return json.load(file)
-        else:
+        elif file_path.endswith(('.yml', '.yaml')):
             return yaml.safe_load(file)
 
 
