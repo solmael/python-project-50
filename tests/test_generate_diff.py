743e52d813141dd7027d8237529267c3cddc4f27
solmael <solmael13@gmail.com>
2025-02-28 12:02:19 +0300

    upd test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,13 +9,26 @@ def read_file(file_name):
     with open(os.path.join(FIXTURES_PATH, file_name), 'r') as f:
         return f.read().strip()
 
-@pytest.mark.parametrize("file1, file2, expected_output", [
-    ('file1.json', 'file2.json', 'expected_json.txt'),
-    ('file1.yml', 'file2.yml', 'expected_yaml.txt')])
+@pytest.mark.parametrize('file1, file2, expected_output', [
+    ('file1.json', 'file2.json', 'expected.txt'),
+    ('file1.yml', 'file2.yml', 'expected.txt'),
+    ('file1.json', 'file2.yml', 'expected.txt'),
+    ('deep_file1.json', 'deep_file2.json', 'deep_expected.txt'),
+    ('deep_file1.yaml', 'deep_file2.yaml', 'deep_expected.txt'),
+    ('deep_file1.json', 'deep_file2.yaml', 'deep_expected.txt')
+    ])
 
 def test_generate_diff(file1, file2, expected_output):
     file1_path = os.path.join(FIXTURES_PATH, file1)
     file2_path = os.path.join(FIXTURES_PATH, file2)
     expected = read_file(expected_output)
     result = generate_diff(file1_path, file2_path)
+    
+    #expected_normalized = '\n'.join(line.strip() for line in expected.splitlines())
+    #result_normalized = '\n'.join(line.strip() for line in result.splitlines())
+    print('\nexpected:')
+    print(expected)
+    print('\nactual:')
+    print(result)
     assert result == expected
+    #assert result_normalized == expected_normalized, f'Mismatch for {file1} and {file2}'
