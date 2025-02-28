a20ae108396a80596f02aed62d39477eb67da4cb
solmael <solmael13@gmail.com>
2025-02-28 12:05:53 +0300

    upd generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,30 +1,9 @@
+from gendiff.build_diff import build_diff
+from gendiff.format_diff import format_diff
 from gendiff.reader import read_files
 
 
-def generate_diff(file1_path, file2_path):
+def generate_diff(file1_path, file2_path, format_name='stylish'):
     file1_data, file2_data = read_files(file1_path, file2_path)
-    
-    all_keys = sorted(set(file1_data.keys()) | set(file2_data.keys()))
-    
-    diff = []
-    for key in all_keys:
-        value1 = format_value(file1_data.get(key))
-        value2 = format_value(file2_data.get(key))
-        
-        if key not in file1_data:
-            diff.append(f'  + {key}: {value2}')
-        elif key not in file2_data:
-            diff.append(f'  - {key}: {value1}')
-        elif file1_data[key] == file2_data[key]:
-            diff.append(f'    {key}: {value1}')
-        else:
-            diff.append(f'  - {key}: {value1}')
-            diff.append(f'  + {key}: {value2}')
-    
-    return '{\n' + '\n'.join(diff) + '\n}'
-
-
-def format_value(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    return str(value)
+    diff = build_diff(file1_data, file2_data)
+    return format_diff(diff)
