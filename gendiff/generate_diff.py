b14891b177704456871f739cc0884f14b71f92da
solmael <solmael13@gmail.com>
2025-03-05 15:39:54 +0300

    upd generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -6,4 +6,4 @@ from gendiff.reader import read_files
 def generate_diff(file1_path, file2_path, format_name='stylish'):
     file1_data, file2_data = read_files(file1_path, file2_path)
     diff = build_diff(file1_data, file2_data)
-    return format_diff(diff)
+    return format_diff(diff, format_name)
