76e43dfc6109789c2c026944c9dd3ce9f6d84517
solmael <solmael13@gmail.com>
2025-03-05 15:38:56 +0300

    tests updated

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,23 +9,30 @@ def read_file(file_name):
     with open(os.path.join(FIXTURES_PATH, file_name), 'r') as f:
         return f.read().strip()
 
-@pytest.mark.parametrize('file1, file2, expected_output', [
-    ('file1.json', 'file2.json', 'expected.txt'),
-    ('file1.yml', 'file2.yml', 'expected.txt'),
-    ('file1.json', 'file2.yml', 'expected.txt'),
-    ('deep_file1.json', 'deep_file2.json', 'deep_expected.txt'),
-    ('deep_file1.yaml', 'deep_file2.yaml', 'deep_expected.txt'),
-    ('deep_file1.json', 'deep_file2.yaml', 'deep_expected.txt')
+@pytest.mark.parametrize('file1, file2, expected_output, format_name', [
+    ('file1.json', 'file2.json', 'expected.txt', 'stylish'),
+    ('file1.yml', 'file2.yml', 'expected.txt', 'stylish'),
+    ('file1.json', 'file2.yml', 'expected.txt', 'stylish'),
+    ('file1.json', 'file2.json', 'plain_expected.txt', 'plain'),
+    ('file1.yml', 'file2.yml', 'plain_expected.txt', 'plain'),
+    ('file1.json', 'file2.yml', 'plain_expected.txt', 'plain'),
+    ('deep_file1.json', 'deep_file2.json', 'deep_expected.txt', 'stylish'),
+    ('deep_file1.yaml', 'deep_file2.yaml', 'deep_expected.txt', 'stylish'),
+    ('deep_file1.json', 'deep_file2.yaml', 'deep_expected.txt', 'stylish'),
+    ('deep_file1.json', 'deep_file2.json', 'deep_plain_expected.txt', 'plain'),
+    ('deep_file1.yaml', 'deep_file2.yaml', 'deep_plain_expected.txt', 'plain'),
+    ('deep_file1.json', 'deep_file2.yaml', 'deep_plain_expected.txt', 'plain')
     ])
 
-def test_generate_diff(file1, file2, expected_output):
+def test_generate_diff(file1, file2, expected_output, format_name):
     file1_path = os.path.join(FIXTURES_PATH, file1)
     file2_path = os.path.join(FIXTURES_PATH, file2)
     expected = read_file(expected_output)
-    result = generate_diff(file1_path, file2_path)
+    result = generate_diff(file1_path, file2_path, format_name)
     
     #expected_normalized = '\n'.join(line.strip() for line in expected.splitlines())
     #result_normalized = '\n'.join(line.strip() for line in result.splitlines())
+    print(f'\nFormat: {format_name}')
     print('\nexpected:')
     print(expected)
     print('\nactual:')
