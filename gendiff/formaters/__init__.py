b7bd66fdec2cd9d2452aae67b6addd728836534b
solmael <solmael13@gmail.com>
2025-03-06 19:01:58 +0300

    added json formater

--- gendiff/formaters/__init__.py
+++ gendiff/formaters/__init__.py
@@ -1,7 +1,9 @@
+from .json import format_json
 from .plain import format_plain
 from .stylish import format_stylish
 
 __all__ = [
+    'format_json',
     'format_stylish',
     'format_plain'
 ]
---
+++ gendiff/formaters/json.py
@@ -0,0 +1,5 @@
+import json as make_json
+
+
+def format_json(diff):
+    return make_json.dumps(diff)
