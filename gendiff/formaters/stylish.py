0e7089646ae0a19ac58bd047185f593dea7d5206
solmael <solmael13@gmail.com>
2025-03-11 11:31:01 +0300

    stylish edit

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -2,44 +2,44 @@ INDENT_SIZE = 4
 
 
 def format_stylish(diff, depth=0):
-    indent = ' ' * (INDENT_SIZE * depth)
+    indent = ' ' * (depth)
     lines = []
     
     for node in diff:
         key = node['key']
         
         if node['type'] == 'nested':
-            value = format_stylish(node['children'], depth + 1)
+            value = format_stylish(node['children'], depth + INDENT_SIZE)
             lines.append(f'{indent}    {key}: {value}')
             
         elif node['type'] == 'unchanged':
-            value = format_value(node['value'], depth + 1)
+            value = format_value(node['value'], depth + INDENT_SIZE)
             lines.append(f'{indent}    {key}: {value}')
             
         elif node['type'] == 'added':
-            value = format_value(node['new_value'], depth + 1)
+            value = format_value(node['new_value'], depth + INDENT_SIZE)
             lines.append(f'{indent}  + {key}: {value}')
             
         elif node['type'] == 'removed':
-            value = format_value(node['old_value'], depth + 1)
-            if value == '':
+            value = format_value(node['old_value'], depth + INDENT_SIZE)
+            if value == "":
                 lines.append(f'{indent}  - {key}:')
             else:
                 lines.append(f'{indent}  - {key}: {value}')
             
         elif node['type'] == 'changed':
-            old_value = format_value(node['old_value'], depth + 1)
-            new_value = format_value(node['new_value'], depth + 1)
-            if old_value == '':
+            old_value = format_value(node['old_value'], depth + INDENT_SIZE)
+            new_value = format_value(node['new_value'], depth + INDENT_SIZE)
+            if old_value == "":
                 lines.append(f'{indent}  - {key}:')
             else:
                 lines.append(f'{indent}  - {key}: {old_value}')
-            if new_value == '':
+            if new_value == "":
                 lines.append(f'{indent}  + {key}:')
             else:
                 lines.append(f'{indent}  + {key}: {new_value}')
     
-    result = '{\n' + '\n'.join(lines) + f'\n{indent}}}'
+    result = '{\n' + '\n'.join(lines) + f'\n{" " * depth}}}'
     return result
 
 
@@ -49,10 +49,10 @@ def format_value(value, depth):
     elif isinstance(value, bool):
         return str(value).lower()
     elif isinstance(value, dict):
-        indent = ' ' * (INDENT_SIZE * depth)
+        indent = ' ' * (depth)
         lines = []
         for k, v in sorted(value.items()):
-            formatted_v = format_value(v, depth + 1)
+            formatted_v = format_value(v, depth + INDENT_SIZE)
             lines.append(f'{indent}    {k}: {formatted_v}')
         result = '{\n' + '\n'.join(lines) + f'\n{indent}}}'
         return result
