817ef7e4584bb1f9839e7ae37df12e8246ba9238
solmael <solmael13@gmail.com>
2025-03-24 19:28:31 +0300

    edited gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python3
-import argparse
 import os
 
 from gendiff import generate_diff
+from gendiff.cli import parsing
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog="gendiff",
-        description="Compares two configuration files and shows a difference.",
-        usage="%(prog)s [-h] [-f FORMAT] first_file second_file",
-        add_help=True
-    )
-
-    parser.add_argument("first_file", 
-                        help="Path to the first configuration file")
-    parser.add_argument("second_file", 
-                        help="Path to the second configuration file")
-
-    parser.add_argument('-f', '--format',
-                        default='stylish',
-                        choices=['stylish', 'plain', 'json'],
-                        help='set format of output')
-
-    args = parser.parse_args()
+    args = parsing()
     possible_paths = [
         os.getcwd(),
         os.path.join(os.path.dirname(__file__), '..', '..', 
@@ -34,10 +17,11 @@ def main():
             full_path = os.path.join(path, filename)
             if os.path.isfile(full_path):
                 return full_path
+        raise FileNotFoundError(f"File {filename} not found")
 
     first_file = find_file(args.first_file)
     second_file = find_file(args.second_file)
-    diff = generate_diff(first_file, second_file, args.format)
+    diff = generate_diff(first_file, second_file, format_name=args.format)
     print(diff)
 
 
