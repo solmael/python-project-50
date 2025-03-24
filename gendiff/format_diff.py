115e733ab297f1e02a265911d5e5f58049e311f5
solmael <solmael13@gmail.com>
2025-03-24 19:29:06 +0300

    edited format_diff

--- gendiff/format_diff.py
+++ gendiff/format_diff.py
@@ -2,11 +2,14 @@ from gendiff.formaters import format_json, format_plain, format_stylish
 
 
 def format_diff(diff, format_name='stylish'):
-    if format_name == 'stylish':
-        return format_stylish(diff)
-    elif format_name == 'plain':
-        return format_plain(diff)
-    elif format_name == 'json':
-        return format_json(diff)
+    formatters = {
+        'stylish': format_stylish,
+        'plain': format_plain,
+        'json': format_json
+    }
+    
+    formatter = formatters.get(format_name)
+    if formatter:
+        return formatter(diff)
     else:
         raise ValueError(f"Unsupported format: {format_name}")
