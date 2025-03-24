3d9a9d22cc520e48b8a9b5c93a80cfabc770f9f9
solmael <solmael13@gmail.com>
2025-03-24 19:25:26 +0300

    move parser

---
+++ gendiff/parser.py
@@ -0,0 +1,14 @@
+import json
+
+import yaml
+
+
+def parse_file(file, format):
+    if not file.strip():
+        return {}
+    
+    match format:
+        case '.json':
+            return json.loads(file)
+        case '.yml' | '.yaml':
+            return yaml.safe_load(file)
