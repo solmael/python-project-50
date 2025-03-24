66923e3bf2ad1c6989f9f4569e57487a1e9259f6
solmael <solmael13@gmail.com>
2025-03-24 19:25:57 +0300

    move cli from scripts

---
+++ gendiff/cli.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def parsing():
+    parser = argparse.ArgumentParser(
+        prog="gendiff",
+        description="Compares two configuration files and shows a difference.",
+        usage="%(prog)s [-h] [-f FORMAT] first_file second_file",
+        add_help=True
+    )
+
+    parser.add_argument("first_file", 
+                        help="Path to the first configuration file")
+    parser.add_argument("second_file", 
+                        help="Path to the second configuration file")
+
+    parser.add_argument('-f', '--format',
+                        default='stylish',
+                        choices=['stylish', 'plain', 'json'],
+                        help='set format of output')
+
+    return parser.parse_args()
