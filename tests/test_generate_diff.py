bfaad5001a23f4aaa1a234231eacf21ca319ad66
solmael <solmael13@gmail.com>
2025-03-31 21:27:48 +0300

    test_generate_diff edited

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -12,74 +12,48 @@ def expected(request):
     with open(os.path.join(FIXTURES_PATH, filename), 'r') as f:
         return f.read().strip()
 
-
+#  уменьшил количество проверок. покрытие, вроде, должно остаться таким же
+#  или нужно как-то иначе их записать?
 @pytest.mark.parametrize('format_name, input_files, expected', [
-    # for stylish
+    # stylish
     ('stylish', 
-     ('file1.json', 
-      'file2.json'), 
-      'expected.txt'),
+     [('file1.json', 'file2.json'),
+      ('file1.yml', 'file2.yml'),
+      ('file1.json', 'file2.yml')], 
+     'expected.txt'),
     ('stylish', 
-     ('file1.yml', 
-      'file2.yml'), 
-      'expected.txt'),
-    ('stylish', 
-     ('file1.json', 
-      'file2.yml'), 'expected.txt'),
-    ('stylish', 
-     ('deep_file1.json', 
-      'deep_file2.json'), 
-      'deep_expected.txt'),
-    ('stylish', 
-     ('deep_file1.yml', 
-      'deep_file2.yml'), 
-      'deep_expected.txt'),
-    
-    # for plain
+     [('deep_file1.json', 'deep_file2.json'),
+      ('deep_file1.yml', 'deep_file2.yml')], 
+     'deep_expected.txt'),
+
+    # plain
     ('plain', 
-     ('file1.json', 
-      'file2.json'), 
-      'plain_expected.txt'),
+     [('file1.json', 'file2.json'),
+      ('file1.yml', 'file2.yml')], 
+     'plain_expected.txt'),
     ('plain', 
-     ('file1.yml', 
-      'file2.yml'), 
-      'plain_expected.txt'),
-    ('plain', 
-     ('deep_file1.json', 
-      'deep_file2.json'), 
-      'deep_plain_expected.txt'),
-    
-    # for json
+     [('deep_file1.json', 'deep_file2.json'),
+      ('deep_file1.yml', 'deep_file2.yml')], 
+     'deep_plain_expected.txt'),
+
+    # json
     ('json', 
-     ('file1.json', 
-      'file2.json'), 
-      'json_expected.txt'),
+     [('file1.json', 'file2.json'),
+      ('file1.yml', 'file2.yml'),
+      ('file1.json', 'file2.yml')], 
+     'json_expected.txt'),
     ('json', 
-     ('deep_file1.yml', 
-      'deep_file2.yml'), 
-      'deep_json_expected.txt'),
-    
-    # mix
+     [('deep_file1.json', 'deep_file2.json'),
+      ('deep_file1.yml', 'deep_file2.yml')], 
+     'deep_json_expected.txt'),
+
+    # empty
     ('stylish', 
-     ('file1.json', 
-      'file2.yml'), 
-      'expected.txt'),
-    ('plain', 
-     ('file1.yml', 
-      'file2.json'), 
-      'plain_expected.txt'),
-    ('json', 
-     ('deep_file1.json', 
-      'deep_file2.yml'), 
-      'deep_json_expected.txt'),
-    
-    # for empty
-    ('stylish', 
-     ('empty1.json', 
-      'empty2.yml'), 
-      'empty_expected.txt'),
+     [('empty1.json', 'empty2.yml')], 
+     'empty_expected.txt'),
 ], indirect=['expected'])
 def test_generate_diff(format_name, input_files, expected):
-    file1 = os.path.join(FIXTURES_PATH, input_files[0])
-    file2 = os.path.join(FIXTURES_PATH, input_files[1])
-    assert generate_diff(file1, file2, format_name) == expected
+    for file1_name, file2_name in input_files:
+        file1 = os.path.join(FIXTURES_PATH, file1_name)
+        file2 = os.path.join(FIXTURES_PATH, file2_name)
+        assert generate_diff(file1, file2, format_name) == expected
