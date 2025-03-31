1da7e6388bdeff19ca5620be6d231b50768e8e2f
solmael <solmael13@gmail.com>
2025-03-31 21:11:35 +0300

    gendiff.py edited

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python3
-import os
-
 from gendiff import generate_diff
-from gendiff.cli import parsing
+from gendiff.cli import get_args
+from gendiff.pathfinder import find_file
 
 
 def main():
-    args = parsing()
-    possible_paths = [
-        os.getcwd(),
-        os.path.join(os.path.dirname(__file__), '..', '..', 
-                     'tests', 'fixtures')]
-
-    def find_file(filename):
-        for path in possible_paths:
-            full_path = os.path.join(path, filename)
-            if os.path.isfile(full_path):
-                return full_path
-        raise FileNotFoundError(f"File {filename} not found")
-
-    first_file = find_file(args.first_file)
-    second_file = find_file(args.second_file)
-    diff = generate_diff(first_file, second_file, format_name=args.format)
+    args = get_args()
+    diff = generate_diff(
+        find_file(args.first_file), 
+        find_file(args.second_file), 
+        args.format
+        )
     print(diff)
 
 
