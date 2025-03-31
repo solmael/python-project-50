320b1dd096747cd29d2d078a2978a5796f3e41fd
solmael <solmael13@gmail.com>
2025-03-31 21:10:33 +0300

    cli edited

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def parsing():
+def get_args():
     parser = argparse.ArgumentParser(
         prog="gendiff",
         description="Compares two configuration files and shows a difference.",
